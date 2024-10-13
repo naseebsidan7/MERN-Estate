51a9584aa230cfc7fdb41aa0e3a0e1b43128e2f1
naseebsidan07 <naseebsidan6@gmail.com>
2024-10-13 17:52:29 +0530

    create sign in api

--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,8 +1,30 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 const SignIn = () => {
+  const [ formData, setFormData ] = useState({})
+  const [ error, setError ] = useState(null)
+  const [ loading, setLoading ] = useState(false)
+  
   return (
-    <div>SignIn</div>
+    <div className='p-3 max-w-lg mx-auto'>
+    <h1 className='text-3xl text-center font-semibold my-7'>Sign In</h1>
+    <form className='flex flex-col gap-4'>
+        <input type="email" placeholder='email' id='email' className='border p-3 rounded-lg'  />
+        <input type="password" placeholder='password' id='password' className='border p-3 rounded-lg'  />
+        <button disabled={loading}  className='bg-slate-700 text-white p-3 rounded-lg uppercase opacity-90 hover:opacity-100 disabled:opacity-80 '>
+             {loading? 'Loading..': 'Sign In'}
+        </button>
+    </form>
+
+    <div className='flex gap-2 mt-5'>
+        <p>New User? </p>
+        <Link to={'/sign-in'}>
+              <span className='text-blue-700'>Sign up</span>
+        </Link>
+    </div>
+    {error && <p className='text-red-500 mt-5'>{error}</p>}
+  </div>
   )
 }
 
