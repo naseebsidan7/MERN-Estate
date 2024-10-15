5932d27ef0ff1dd49b3932b51614a06f4561e006
naseebsidan07 <naseebsidan6@gmail.com>
2024-10-16 00:34:24 +0530

    add redux persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,12 +1,24 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { combineReducers, configureStore } from '@reduxjs/toolkit'
 import userReducer from './user/userSlice'
+import { persistReducer, persistStore } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
+const rootReducer = combineReducers({ user: userReducer })
  
+const persistConfig = {
+      key: 'root',
+      storage,
+      version: 1
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 export const store = configureStore({ 
-    reducer: {user: userReducer }, 
+    reducer: persistedReducer, 
     middleware: (getDefaultMiddleware) =>
       getDefaultMiddleware({
             serializableCheck: false
       }),
 })
 // The store now has redux-thunk added and the Redux DevTools Extension is turned on
+
+export const persistor = persistStore(store)
