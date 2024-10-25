8e493a13e7f56c278d88aa4d8b48b1698c013f07
naseebsidan07 <naseebsidan6@gmail.com>
2024-10-26 00:14:56 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -5,6 +5,7 @@ import userRouter from './routes/user.route.js'
 import authRouter from './routes/auth.route.js'
 import listingRouter from './routes/listing.route.js'
 import cookieParser from 'cookie-parser'
+import path from 'path'
 
 dotenv.config()
 
@@ -13,6 +14,9 @@ mongoose.connect(process.env.MONGO).then(() => {
 }).catch((err) => {
       console.log(err)
 })
+
+const __dirname = path.resolve();
+
 
 const app = express()
 
@@ -25,6 +29,13 @@ app.listen(3000, () => console.log('server is running on port 3000 '))
 app.use('/api/user', userRouter)
 app.use('/api/auth', authRouter)
 app.use('/api/listing', listingRouter)
+
+
+app.use(express.static(path.join(__dirname, 'client/dist')))
+
+app.get('*', (req, res) => {
+      res.sendFile(path.join(__dirname, 'client/dist/index.html'))
+})
 
 app.use((err, req, res, next ) => {
       const statusCode = err.statusCode || 300;
