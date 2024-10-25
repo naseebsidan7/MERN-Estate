9a13cabde01b0f7c5761121aac113c8464ae2c39
naseebsidan07 <naseebsidan6@gmail.com>
2024-10-26 00:03:37 +0530

    complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,12 @@
  
 const About = () => {
   return (
-    <div>About</div>
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-800 ">About Sidan Estate</h1>
+      <p className="mb-4 text-slate-700 ">Sidan Estate was founded with a vision to transform property ownership and investment into a seamless and rewarding experience. With a deep understanding of the local market and a passion for quality, we are committed to providing a full spectrum of real estate services. Our team of seasoned professionals guides clients through every step of the buying, selling, and renting process, delivering exceptional outcomes and making property dreams a reality.</p>
+      <p className="mb-4 text-slate-700 ">At Sidan Estate, our mission is to foster trustworthy relationships through transparency, innovation, and customer-centric solutions. We believe that everyone deserves access to valuable and stress-free real estate experiences. By staying up-to-date with market trends and leveraging cutting-edge technology, we strive to offer our clients top-tier services, helping them make informed decisions that support their lifestyle and investment goals.</p>
+      <p className="mb-4 text-slate-700 ">Choosing Sidan Estate means choosing a partner dedicated to excellence. From our wide range of properties to our personalized approach, we put the needs of our clients first. Whether you're looking to buy, sell, or rent, our team is here to provide tailored advice, local insights, and unmatched dedication to help you achieve the best results in the real estate market.</p>
+    </div>
   )
 }
 
